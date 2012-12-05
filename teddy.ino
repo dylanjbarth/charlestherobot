d7fffcd8992e37b2ea5ac30a1c8e14f23c4efc76
Dylan Barth <dylan.j.barth@gmail.com>
2012-12-05 09:48:01 -0800

    Added calibration logic. Next step is to try and average out some human voices. Also would be good to link in a statusLED to show when it is calibrating and when it's ok to be loud.

--- teddy.ino
+++ teddy.ino
@@ -18,9 +18,10 @@ Global Variables
 int calibrationButton = 7;
 int micPin = A0;
 
-bool silence = false;
+bool calibrated = false;
 
-int calibrationArray = new Array();
+// const ints
+int MY_SIZE = 199;
 
 
 /* ====================================================================
@@ -37,13 +38,10 @@ Void Loop
 ==================================================================== */
 
 void loop() {
-	// While user is calibrating, gather test data
-	while (checkForSilence()) {
-
+	// First time program starts, force a calibration
+	if (calibrated == false) {
+		int silenceThreshold = calibrate();
 	}
-
-
-		// after exiting while loop, average values into a 'calibrated silence int'
 
 	// with silence as baseline, take measurements 
 
@@ -62,6 +60,33 @@ void loop() {
 Functions
 ==================================================================== */
 
+int calibrate() {
+	"""
+	calibrate returns a int that represents the average level of noise in the surrounding environment. 
+	The idea is to set a noise threshold so that human speech can be differentiated from the background. 
+	Note: There are major flaws with this strategy =(
+	"""
+	// wait for user to press calibration button
+	while (checkForSilence() == false) {
+		continue;
+	}
+
+	// array of 'silent' input - sized at 200
+	int silenceArray[MY_SIZE];
+	int counter = 0;
+	// record input while calibration button is pressed
+	while (checkForSilence() == true) {
+		int micInput = digitalRead(micPin);
+		// only update array for first 200 values
+		if (counter < MY_SIZE) {
+			silenceArray = addToList(silenceArray, micInput);
+		}
+		counter ++;
+	}
+	// finally average array, return a silence threshold
+	return int threshold = findAverage(silenceArray);
+}
+
 bool checkForSilence() {
 	"""
 	checkForSilence returns a boolean variable indicating whether or not the user 
@@ -75,3 +100,31 @@ bool checkForSilence() {
 	}
 }
 
+int addToList(list, input) {
+	"""
+	addToList takes two variables, an array and a value to be added. Finds the first 0
+	space in the array and adds to value there, returning the new array.
+	"""
+	int counter = 0;
+	int val = 1;
+	// index through the array until it finds a 0 
+	while (val != 0) {
+		val = list[counter];
+		counter ++;
+	}
+	// when it breaks, fill in this array value with desired value
+	list[counter] = input;
+	return list;
+}
+
+int findAverage(list) {
+	"""
+	findAverage take the list and returns the average value in the list.
+	"""
+	int counter = 0;
+	int total = 0;
+	while (counter < MY_SIZE) {
+		total += list[counter];
+	}
+	return int avg = total / MY_SIZE;
+}
