b8ba19ee2a548a81172d6d410f8df9f612584ef1
dylanjbarth <dylan.j.barth@gmail.com>
2012-12-09 20:14:45 -0800

    Ok trying a new direction with the audio sampling. Everything working except responding at the correct times.

--- teddy2.ino
+++ teddy2.ino
@@ -35,15 +35,16 @@ bool talking = false;
 
 // const ints
 #define MY_SIZE 500 // sample time length for calibrating silence
-#define MEASURE 50 // sample time length for measuring talking
+#define MEASURE 32 // sample time length for measuring talking
 #define TALKING_MEASURES 2 // # of measures for talking threshold
 #define FILE_COUNT 11 // number of WAV files on SD card
 #define error(msg) error_P(PSTR(msg))
 
 // variable ints
 int timesAboveThreshold = 0; // a measure of number of intervals talking is true
-int averageSilence = 0;
-int measureAverage = 0;
+int averageSilence = 0; // used to hold avg silence values
+int measureAverage = 0; // used to hold mic sampling values
+int lastResponse = 0; // used so that responses aren't repeated
 
 
 /* ====================================================================
@@ -70,7 +71,7 @@ void setup() {
 	// Now we will look for a FAT partition!
 	uint8_t part;
 	for (part = 0; part < 5; part++) {     // we have up to 5 slots to look in
-	if (vol.init(card, part)) 
+		if (vol.init(card, part)) 
 	  break;                             // we found one, lets bail
 	}
 	if (part == 5) {                       // if we ended up not finding one  :(
@@ -104,92 +105,119 @@ Void Loop
 
 void loop() {
 	// Part 1: Calibration
-	if (calibrated == false) {
-		// 0. wait for Charles to tell us to calibrate.
-		Serial.println("Loading...");
-		delay(3000);
-		playcomplete("1.wav");
-		// 1. store a reading of values over time: MY_SIZE
-		// but first an initial reading:
-		averageSilence = analogRead(micPin);
-		int counter = 0;
-		while (counter <= MY_SIZE) {
-			int temp = analogRead(micPin);
-			averageSilence = (averageSilence + temp)/2; // averaging each time
-			counter ++;
-			Serial.print("avg silence is ");
-			Serial.print(averageSilence);
-			Serial.print(" in round ");
-			Serial.println(counter);
-		}
-		Serial.println("End of calibration check.");
-		Serial.print("Average silence is: ");
-		Serial.println(averageSilence);
-  
-		calibrated = true;
-		playcomplete("2.wav");
-	}
+	// if (calibrated == false) {
+	// 	// 0. wait for Charles to tell us to calibrate.
+	// 	Serial.println("Loading...");
+	// 	delay(3000);
+	// 	playcomplete("1.wav");
+	// 	// 1. store a reading of values over time: MY_SIZE
+	// 	// but first an initial reading:
+	// 	averageSilence = analogRead(micPin);
+	// 	int counter = 0;
+	// 	while (counter <= MY_SIZE) {
+	// 		int temp = analogRead(micPin);
+	// 		averageSilence = (averageSilence + temp)/2; // averaging each time
+	// 		counter ++;
+	// 		Serial.print("avg silence is ");
+	// 		Serial.print(averageSilence);
+	// 		Serial.print(" in round ");
+	// 		Serial.println(counter);
+	// 	}
+	// 	Serial.println("End of calibration check.");
+	// 	Serial.print("Average silence is: ");
+	// 	Serial.println(averageSilence);
+
+	// 	calibrated = true;
+	// 	playcomplete("2.wav");
+	// }
+	// #define SILENCE averageSilence // set it constant for the rest of the program
+	#define SILENCE 340 // for speeding up debugging
 
 
 	// Part 2: Identifying Speech
 	// A. Take measures until reading at least two talking measurements 
-	do {
-		// 0. create an array of measured values
-		int counter = 0;
-		int measureArray[MEASURE];
-		while (counter < MEASURE) {
-			measureArray[counter] = analogRead(micPin);
-			counter ++;
-		}
+	// do {
+	// 	// 0. take a sampling of the volume level in increments of MEASURE
+	// 	int counter = 0;
+	// 	while (counter < MEASURE) {
+	// 		measureAverage = (measureAverage + analogRead(micPin))/2;
+	// 		counter ++;
+	// 		Serial.print("avg volume is ");
+	// 		Serial.print(measureAverage);
+	// 		Serial.print(" in round ");
+	// 		Serial.println(counter);
+	// 	}
+	// 	Serial.println("End of measure check.");
+	// 	Serial.print("Average volume is: ");
+	// 	Serial.println(measureAverage);
 
-		// 1. average values
-		int sum = 0;
-		for (int i=0; i<MEASURE; i++) {
-			sum += measureArray[i];
-		}
-		measureAverage = sum/MEASURE;
+	// 	// 1. compare this to silence threshold
+	// 	if (measureAverage > SILENCE+10) {
+	// 		Serial.println("Greater than silence!");
+	// 		timesAboveThreshold ++;
+	// 	}
+	// 	if (timesAboveThreshold > TALKING_MEASURES) {
+	// 		talking = true;
+	// 	}
+	// } while ((talking == false) || (measureAverage <= SILENCE)); // will exit loop when talking is true AND silence is achieved
+	
+	// // B. Reset measurement tools
+	// timesAboveThreshold = 0; // reset counter
+	// talking = false; // reset flag
+	// measureAverage = analogRead(micPin); // reset measuring
 
-		// 2. compare this to silence threshold
-		if (measureAverage > averageSilence) {
-			timesAboveThreshold ++;
-		}
-		if (timesAboveThreshold > TALKING_MEASURES) {
-			talking = true;
-		}
-
-
-	} while ((talking == false) || (measureAverage <= averageSilence)); // will exit loop when talking is true AND silence is achieved
+	// // C. Debugging
+	// Serial.print("I would be RESPONDING NOW! because: ");
+	// if ((talking != false) && (measureAverage > SILENCE)){
+	// 	Serial.println("talking != false and measureAvg is greater than SILENCE");
+	// } else if (talking != false){
+	// 	Serial.println("talking != false");
+	// } else if (measureAverage > SILENCE){
+	// 	Serial.println("measureAvg is greater than SILENCE");
+	// } else {
+	// 	Serial.println("Major problems are happening. give up now.");
+	// }
 
 	// Part 3: Responding
-	// switch (random(1, 9)) {
-	//     case 1:
-	//       playcomplete("3.wav");
-	//       break;
-	//     case 2:
-	//       playcomplete("4.wav");
-	//       break;
-	//     case 3:
-	//       playcomplete("5.wav");
-	//       break;
-	//     case 4:
-	//       playcomplete("6.wav");
-	//       break;
-	//     case 5:
-	//       playcomplete("7.wav");
-	//       break;
-	//     case 6:
-	//       playcomplete("8.wav");
-	//       break;
-	//     case 7:
-	//       playcomplete("9.wav");
-	//       break;
-	//     case 8:
-	//       playcomplete("10.wav");
-	//       break;
-	//     case 9:
-	//       playcomplete("11.wav");
-	//       break;
-	//   }
+	// pick a random response not the same as the previous
+	while (true) {
+		int temp = random(1, 9);
+		if (temp != lastResponse) {
+			lastResponse = temp;
+			break;
+		}
+	}
+	// play random response
+	switch (temp) { 
+		case 1:
+			playcomplete("3.wav");
+			break;
+		case 2:
+			playcomplete("4.wav");
+			break;
+		case 3:
+			playcomplete("5.wav");
+			break;
+		case 4:
+			playcomplete("6.wav");
+			break;
+		case 5:
+			playcomplete("7.wav");
+			break;
+		case 6:
+			playcomplete("8.wav");
+			break;
+		case 7:
+			playcomplete("9.wav");
+			break;
+		case 8:	
+			playcomplete("10.wav");
+			break;
+		case 9:
+			playcomplete("11.wav");
+			break;
+	}
+	// restart loop
 }
 
 /* ====================================================================
@@ -202,74 +230,74 @@ Functions
 /*
  * print error message and halt
  */
-void error_P(const char *str) {
-  PgmPrint("Error: ");
-  SerialPrint_P(str);
-  sdErrorCheck();
-  while(1);
-}
+ void error_P(const char *str) {
+ 	PgmPrint("Error: ");
+ 	SerialPrint_P(str);
+ 	sdErrorCheck();
+ 	while(1);
+ }
 /*
  * print error message and halt if SD I/O error, great for debugging!
  */
-void sdErrorCheck(void) {
-  if (!card.errorCode()) return;
-  PgmPrint("\r\nSD I/O error: ");
-  Serial.print(card.errorCode(), HEX);
-  PgmPrint(", ");
-  Serial.println(card.errorData(), HEX);
-  while(1);
-}
+ void sdErrorCheck(void) {
+ 	if (!card.errorCode()) return;
+ 	PgmPrint("\r\nSD I/O error: ");
+ 	Serial.print(card.errorCode(), HEX);
+ 	PgmPrint(", ");
+ 	Serial.println(card.errorData(), HEX);
+ 	while(1);
+ }
 
 // Plays a full file from beginning to end with no pause.
-void playcomplete(char *name) {
+ void playcomplete(char *name) {
   // call our helper to find and play this name
-	Serial.println("before playfile");
-	Serial.println(name);
-    playfile(name);
-	Serial.println("after playfile");
-  while (wave.isplaying) {
+ 	Serial.println("before playfile");
+ 	Serial.println(name);
+ 	playfile(name);
+ 	Serial.println("after playfile");
+ 	while (wave.isplaying) {
   // do nothing while its playing
-  }
+ 	}
   // now its done playing
-}
+ }
 
-void playfile(char *name) {
-	Serial.println("inside playfile()");
+ void playfile(char *name) {
+ 	Serial.println("inside playfile()");
   // see if the wave object is currently doing something
   if (wave.isplaying) {// already playing something, so stop it!
     wave.stop(); // stop it
     Serial.println("wave.stop()");
-  }
-  Serial.println("past wave.isplaying check");
+}
+Serial.println("past wave.isplaying check");
   // look in the root directory and open the file
-  if (!f.open(root, name)) {
-    putstring("Couldn't open file "); Serial.print(name); return;
-  } 
-  Serial.println("past cannot open file check");
+if (!f.open(root, name)) {
+	putstring("Couldn't open file "); Serial.print(name); return;
+} 
+Serial.println("past cannot open file check");
   // OK read the file and turn it into a wave object
-  if (!wave.create(f)) {
-    putstring_nl("Not a valid WAV"); return;
-  }
-  Serial.println("past create wav file check");
-  
+if (!wave.create(f)) {
+	putstring_nl("Not a valid WAV"); return;
+}
+Serial.println("past create wav file check");
+
   // ok time to play! start playback
-  wave.play();
-  Serial.println("wave.play()");
+wave.play();
+Serial.println("wave.play()");
 }
 
 // this handy function will return the number of bytes currently free in RAM, great for debugging!   
 int freeRam(void)
 {
-  extern int  __bss_end; 
-  extern int  *__brkval; 
-  int free_memory; 
-  if((int)__brkval == 0) {
-    free_memory = ((int)&free_memory) - ((int)&__bss_end); 
-  }
-  else {
-    free_memory = ((int)&free_memory) - ((int)__brkval); 
-  }
-  return free_memory; 
+	extern int  __bss_end; 
+	extern int  *__brkval; 
+	int free_memory; 
+	if((int)__brkval == 0) {
+		free_memory = ((int)&free_memory) - ((int)&__bss_end); 
+	}
+	else {
+		free_memory = ((int)&free_memory) - ((int)__brkval); 
+	}
+	return free_memory; 
 } 
 
 // ********* end WaveHC example code: *************************** //
